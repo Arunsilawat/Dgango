bbc57671f321f077b5196a5a5097fefe65711869
Arun silawat <arunsilawat722362@gmail.com>
2024-09-07 18:09:29 +0530

    Template outcutting

--- Template_cutting/project/app/urls.py
+++ Template_cutting/project/app/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import path,include
 from app import views
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('home/',views.home, name='home'),
 ]
