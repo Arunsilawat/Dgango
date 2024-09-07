02b1f17edf81202567f34c89d6b3916718dbafc8
Arun silawat <arunsilawat722362@gmail.com>
2024-09-07 16:52:27 +0530

    Template outcutting

--- Fileoutsite/project/project/settings.py
+++ Fileoutsite/project/project/settings.py
@@ -15,7 +15,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-TEMPLATES_DIRS=os.path.join(BASE_DIR,'mytemplates')
+TEMPLATES_DIRS=os.path.join(BASE_DIR,'mytemplates')  #path-------
+static_path=os.path.join(BASE_DIR,'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -117,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILE_DIRS=[static_path]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
