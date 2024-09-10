02c2d0c96c459bcca011b8b1de2459401cf035bc
Arun silawat <arunsilawat722362@gmail.com>
2024-09-10 15:33:17 +0530

    data use and if else

--- Template_cutting/project/app/views.py
+++ Template_cutting/project/app/views.py
@@ -6,8 +6,8 @@ def home(request):
     # data2={'name':'Rahul','age':20,'city':'Betul'}
     # data3={'name':'Sonam','age':21,'city':'Jabalpur'}
     # data4={'name':'Mukesh','age':29,'city':'Jaipur'}
-    data1={}
-    data2={'name':'Mukesh','age':29,'city':'Jaipur'}
+    data1={'name':'Mukesh','age':29,'city':'Jaipur'}
+    data2={}
     data3={}
     data4={}
     return render(request,'index.html',{'key1':data1,'key2':data2,'key3':data3,'key4':data4})
