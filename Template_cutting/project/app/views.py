e0001d40173613da443b3b761963e0ad1d27097e
Arun silawat <arunsilawat722362@gmail.com>
2024-09-10 15:18:52 +0530

    data use and if else

--- Template_cutting/project/app/views.py
+++ Template_cutting/project/app/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
- 
+    # data1={'name':'Arun','age':24,'city':'Bhopal'}
+    # data2={'name':'Rahul','age':20,'city':'Betul'}
+    # data3={'name':'Sonam','age':21,'city':'Jabalpur'}
+    # data4={'name':'Mukesh','age':29,'city':'Jaipur'}
+    data1={}
+    data2={'name':'Mukesh','age':29,'city':'Jaipur'}
+    data3={}
+    data4={}
+    return render(request,'index.html',{'key1':data1,'key2':data2,'key3':data3,'key4':data4})
