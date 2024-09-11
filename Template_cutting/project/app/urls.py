530b36ad3759eac2c360e4a6e15ac240d403ca10
Arun silawat <arunsilawat722362@gmail.com>
2024-09-11 15:34:03 +0530

    collection

--- Template_cutting/project/app/urls.py
+++ Template_cutting/project/app/urls.py
@@ -3,4 +3,5 @@ from django.urls import path,include
 from app import views
 urlpatterns = [
     path('home/',views.home, name='home'),
+    path('collection/',views.collection, name='collection'),
 ]
--- Template_cutting/project/app/views.py
+++ Template_cutting/project/app/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
+# def home(request):
     # data1={'name':'Arun','age':24,'city':'Bhopal'}
     # data2={'name':'Rahul','age':20,'city':'Betul'}
     # data3={'name':'Sonam','age':21,'city':'Jabalpur'}
     # data4={'name':'Mukesh','age':29,'city':'Jaipur'}
-    data1={'name':'Mukesh','age':29,'city':'Jaipur'}
-    data2={}
-    data3={}
-    data4={}
-    return render(request,'index.html',{'key1':data1,'key2':data2,'key3':data3,'key4':data4})
+    # data1={'name':'Mukesh','age':29,'city':'Jaipur'}
+    # data2={}
+    # data3={}
+    # data4={}
+    # return render(request,'index.html',{'key1':data1,'key2':data2,'key3':data3,'key4':data4})
+def home(request):
+    return render(request,'home.html')
+def collection(request):
+    data=[{'name':'Arun','age':24,'city':'Bhopal'},
+          {'name':'Aman','age':20,'city':'Indore'},
+          {'name':'Aman','age':20,'city':'Indore'},
+          {'name':'Aman','age':20,'city':'Indore'},
+          {'name':'Aman','age':20,'city':'Indore'},
+          {'name':'Aman','age':20,'city':'Indore'},
+          {'name':'Arun','age':24,'city':'Bhopal'},
+          {'name':'Aman','age':20,'city':'Indore'},
+          {'name':'Aman','age':20,'city':'Indore'},
+          {'name':'Aman','age':20,'city':'Indore'},
+          {'name':'Aman','age':20,'city':'Indore'},
+          {'name':'Aman','age':20,'city':'Indore'}
+          ]
+    return render(request,'collection.html',{'data':data})
