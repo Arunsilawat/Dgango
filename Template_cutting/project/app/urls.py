30210d3d982ed4e9f78224c6b3d70124fec4b230
Arun silawat <arunsilawat722362@gmail.com>
2024-09-24 15:36:08 +0530

    Session

--- Template_cutting/project/app/urls.py
+++ Template_cutting/project/app/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import path,include
 from app import views
 urlpatterns = [
-    path('home/',views.home, name='home'),
+    path('',views.home, name='home'),
     path('collection/',views.collection, name='collection'),
     path('register/',views.register, name='register'),
     path('login/',views.login, name='login'), 
     path('registerdata/',views.registerdata, name='registerdata'), 
-    # path('userlogin/',views.userlogin, name='userlogin'), 
+    path('logout/',views.logout, name='logout'), 
 ]
--- Template_cutting/project/app/views.py
+++ Template_cutting/project/app/views.py
@@ -21,43 +21,44 @@ def collection(request):
           {'name':'Aman','age':20,'city':'Indore'},
           {'name':'Aman','age':20,'city':'Indore'},
           {'name':'Arun','age':24,'city':'Bhopal'},
-          {'name':'Aman','age':20,'city':'Indore'},
-          {'name':'Aman','age':20,'city':'Indore'},
-          {'name':'Aman','age':20,'city':'Indore'},
-          {'name':'Aman','age':20,'city':'Indore'},
-          {'name':'Aman','age':20,'city':'Indore'}
+          {'name':'ARun','age':20,'city':'Indore'},
+          {'name':'ARun','age':20,'city':'Indore'},
+          {'name':'ARun','age':20,'city':'Indore'},
+          {'name':'ARun','age':20,'city':'Indore'},
+          {'name':'ARun','age':20,'city':'Indore'}
           ]
     return render(request,'collection.html',{'data':data})
-def register(request):
-    return render(request,'register.html')
+# ------------------------cookies--------------------------------------------------------------
+# def register(request):
+#     return render(request,'register.html')
    
-def login(request):
-    if request.method=='POST':
-        email1=request.POST.get('email')
-        password1=request.POST.get('password')
-        # print(email,password)
-        name=request.COOKIES['name']
-        email=request.COOKIES['email']
-        contact=request.COOKIES['contact']
-        password=request.COOKIES['password']
-        if email1==email:
-            if password1==password:
-                data={'name':name,
-                      'email':email,
-                      'contact':contact,
-                      'password':password
-                      }
-                return render(request,'dashboard.html',data)
-            else:
-                msg='email id and password not matched'
-                return render(request,'login.html',{'msg':msg})
+# def login(request):
+#     if request.method=='POST':
+#         email1=request.POST.get('email')
+#         password1=request.POST.get('password')
+#         # print(email,password)
+#         name=request.COOKIES['name']
+#         email=request.COOKIES['email']
+#         contact=request.COOKIES['contact']
+#         password=request.COOKIES['password']
+#         if email1==email:
+#             if password1==password:
+#                 data={'name':name,
+#                       'email':email,
+#                       'contact':contact,
+#                       'password':password
+#                       }
+#                 return render(request,'dashboard.html',data)
+#             else:
+#                 msg='email id and password not matched'
+#                 return render(request,'login.html',{'msg':msg})
                 
 
-        else:
-            msg='email id not register'
-            return render(request,'login.html',{'msg':msg})
-    else:
-        return render(request,'login.html')
+#         else:
+#             msg='email id not register'
+#             return render(request,'login.html',{'msg':msg})
+#     else:
+#         return render(request,'login.html')
 
 def registerdata(request):
     # print(request.method)
@@ -87,8 +88,50 @@ def registerdata(request):
 #         password=request.POST.get('password')
 #         print(email,password)
     
-
-
-        
-    
-    
+def logout(request):
+    response=render(request,'home.html')
+    response.delete_cookie('name')
+    response.delete_cookie('email')
+    response.delete_cookie('contact')
+    response.delete_cookie('password')
+    return response
+# ---------------------------end cookies---------------------------------------------------
+def register(request):
+    if request.method=='POST':
+        name=request.POST.get('nm')
+        email=request.POST.get('em')
+        contact=request.POST.get('con')
+        password=request.POST.get('pass')
+        data={'name':name,
+              'email':email,
+              'contact':contact,
+              'password':password,
+              }
+        request.session['data']=data
+        return render(request,'login.html')
+    else:
+        return render(request,'register.html')
+def login(request):
+    if request.method=='POST':
+        email1=request.POST.get('email')
+        password1=request.POST.get('password')
+        data=request.session.get('data')
+        print(data)
+        print(data["name"],data["email"],data["contact"],data["password"])
+        # if email1==email:
+        #     if password1==password:
+        #         data={'name':name,
+        #               'email':email,
+        #               'contact':contact,
+        #               'password':password
+        #               }
+        #         return render(request,'dashboard.html',data)
+        #     else:
+        #         msg='email id and password not matched'
+        #         return render(request,'login.html',{'msg':msg})
+ 
+        # else:
+        #     msg='email id not register'
+        #     return render(request,'login.html',{'msg':msg})
+    else:
+        return render(request,'login.html')
