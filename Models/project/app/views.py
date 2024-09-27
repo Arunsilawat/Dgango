bf9d7882a79ac8500ac6d2bb7a8e99a151177199
Arun silawat <arunsilawat722362@gmail.com>
2024-09-27 15:38:07 +0530

    model manager

--- Models/project/app/views.py
+++ Models/project/app/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-
+from.models import Student
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+def register(request):
+    if request.method=='POST':
+        name=request.POST.get('nm')
+        email=request.POST.get('em')
+        contact=request.POST.get('con')
+        password=request.POST.get('pass')
+        #print(name,email,contact,password)
+        Student.objects.create(stu_name=name,stu_email=email,stu_contact=contact,stu_password=password)
+        msg="registraton successfull"
+        return render(request,'home.html',{'msg':msg})
+    else:
+     return render(request,'register.html')
+
+def login(request):
+    return render(request,'login.html')
