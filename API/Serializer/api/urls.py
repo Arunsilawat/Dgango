c72156ff442a8aa8ac21dec53126f777cbe4a80c
Arun silawat <arunsilawat722362@gmail.com>
2024-11-08 13:55:27 +0530

    get

--- API/Serializer/api/urls.py
+++ API/Serializer/api/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("stu_info/<int:pk>",student_detail,name='name'),
-    path("stu_list/",student_list,name='student_list'),
+    # path("stu_info/<int:pk>",student_detail,name='name'),
+    # path("stu_list/",student_list,name='student_list'),
+    path("student_api/",student_api,name='student_api'),
 ]
